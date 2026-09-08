fa6a6c02f8764c726a89bb7136dde2470895b0eb
rishabh <rishabhkashyap87134@gmail.com>
2026-09-09 00:03:57 +0530

    fix: Wrap app in CartProvider

--- planthub/src/App.jsx
+++ planthub/src/App.jsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './hooks/useCart';
 
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
     </AuthProvider>
   );
 }
